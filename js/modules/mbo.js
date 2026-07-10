dd8ce68ce634bffbe1745b8b8db56d412e06195b
tigertamvip <tigertamvip@me.com>
2026-07-10 21:58:52 +0800

    V0.5.150: 回退到V0.5.147方案-修复列宽错位+恢复表头粘滞

--- js/modules/mbo.js
+++ js/modules/mbo.js
@@ -2016,12 +2016,9 @@ function renderWPTable(plan){
     else newTasks.push(j);
   }
 
-  var colgroupHtml='<colgroup><col style="width:56px"><col style="width:180px"><col style="width:80px"><col style="width:115px"><col style="width:115px"><col style="width:115px"><col style="width:56px"><col style="width:90px"><col style="width:48px"><col style="width:80px"><col style="width:150px"><col style="width:90px"><col style="width:56px"><col style="width:200px"><col></colgroup>';
-  html+='<div class="wp-table-area">';
-  html+='<table class="wp-table wp-table-head">'+colgroupHtml+'<thead><tr>';
+  html+='<div class="wp-table-area"><div class="wp-table-wrap"><table class="wp-table"><colgroup><col style="width:56px"><col style="width:180px"><col style="width:80px"><col style="width:115px"><col style="width:115px"><col style="width:115px"><col style="width:56px"><col style="width:90px"><col style="width:48px"><col style="width:80px"><col style="width:150px"><col style="width:90px"><col style="width:56px"><col style="width:200px"><col></colgroup><thead><tr>';
   html+='<th class="col-num">#</th><th class="col-work">本周重点工作</th><th class="col-goal">优先级</th><th class="col-hours">启动日期</th><th class="col-hours">计划完成日期</th><th class="col-hours">实际完成日期</th><th class="col-hours dur-tooltip" style="min-width:80px">计划/实际耗时</th><th class="col-status">完成状态</th><th class="col-score">积分</th><th class="col-supporters">协同人</th><th class="col-wide">遇到的问题/挑战</th><th class="col-problemtype">问题类型</th><th class="col-needboss">需上级介入</th><th class="col-remarks">备注说明</th><th class="col-boss" style="white-space:normal;overflow:visible">上级评价与建议</th>';
-  html+='</tr></thead></table>';
-  html+='<div class="wp-table-wrap"><table class="wp-table">'+colgroupHtml+'<tbody>';
+  html+='</tr></thead><tbody>';
 
   var seq=0;
 
@@ -2136,7 +2133,7 @@ function renderWPTable(plan){
   html+='<td class="wp-total-num" style="color:'+(overdue>0?'var(--danger)':'var(--success)')+'">'+overdue+' 项</td>'; // 问题类型
   html+='<td colspan="3" style="background:#F5F6F8"></td>'; // 需上级+备注+上级评价
   html+='</tr>';
-  html+='</tbody></table></div></div>';
+  html+='</tbody></table></div>';
 
   // ===== 协同任务区域（独立表格，来自其他同事的协同请求）=====
   html+=_renderCollabTasksSection(plan);
@@ -2222,17 +2219,6 @@ function renderWPTable(plan){
   html+='</div>';
 
   content.insertAdjacentHTML('beforeend',html);
-
-  // V0.5.149: 同步 thead 和 tbody 水平滚动
-  setTimeout(function(){
-    var wrap=content.querySelector('.wp-table-wrap');
-    var head=content.querySelector('.wp-table-head');
-    if(wrap&&head){
-      wrap.addEventListener('scroll',function(){
-        head.style.transform='translateX(-'+wrap.scrollLeft+'px)';
-      });
-    }
-  },10);
 
   // ★ V0.5.67: 绑定拖拽事件
   setTimeout(function(){_bindWPDragEvents();},50);
